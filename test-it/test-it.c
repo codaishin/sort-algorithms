7f4373c286d27aaa3a8f91236c8e16c8b6240bcd
codaishin <codaishin@gmail.com>
2020-12-06 12:45:21 +0000

    test-it.c: Formatting

--- test-it/test-it.c
+++ test-it/test-it.c
@@ -4,12 +4,15 @@
 
 #define ARR_SIZE 8
 
-void sort(int* array, size_t length)
+void sort(int *array, size_t length)
 {
-  for (int i = 0; i < length - 1; ++i) {
-		for (int j = 1; j < length - i; ++j) {
-			if (array[j-1] > array[j]) {
-				SWAP(array[j-1], array[j]);
+	for (int i = 0; i < length - 1; ++i)
+	{
+		for (int j = 1; j < length - i; ++j)
+		{
+			if (array[j - 1] > array[j])
+			{
+				SWAP(array[j - 1], array[j]);
 			}
 		}
 	}
@@ -17,8 +20,8 @@ void sort(int* array, size_t length)
 
 int main()
 {
-	int actual[ARR_SIZE] = { 4, 2, -1, 3, 24, 42, -5, 2 };
-	int expected[ARR_SIZE] = { -5, -1, 2, 2, 3, 4, 24, 42 };
+	int actual[ARR_SIZE] = {4, 2, -1, 3, 24, 42, -5, 2};
+	int expected[ARR_SIZE] = {-5, -1, 2, 2, 3, 4, 24, 42};
 
 	sort(actual, ARR_SIZE);
 
