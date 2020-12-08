21759c9cf0d09c17065f50b27e04042a984ac698
codaishin <codaishin@gmail.com>
2020-12-08 14:45:43 +0000

    2020/tools: Added string array assert function

--- 2020/tools/tools.c
+++ 2020/tools/tools.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "tools.h"
 
 #define BUFFSIZE 8
@@ -25,6 +26,16 @@ void array_to_console(int* array, size_t size)
 	printf("%i]\n", array[i]);
 }
 
+void str_array_to_console(char** array, size_t size)
+{
+	printf("[");
+	size_t i;
+	for (i = 0; i < size - 1; ++i) {
+		printf("%s, ", array[i]);
+	}
+	printf("%s]\n", array[i]);
+}
+
 void assert_equal(int* expected, int* actual, size_t size, const char* funcname)
 {
 	int okay = 1;
@@ -35,6 +46,24 @@ void assert_equal(int* expected, int* actual, size_t size, const char* funcname)
 			array_to_console(expected, size);
 			printf(" └── but was: ");
 			array_to_console(actual, size);
+			okay = 0;
+		}
+	}
+	if (okay) {
+		printf("[%s✓%s] %s\n", colors.green, colors.reset, funcname);
+	}
+}
+
+void assert_str_equal(char** expected, char** actual, size_t size, const char* funcname)
+{
+	int okay = 1;
+	for (size_t i = 0; i < size && okay; ++i) {
+		if (strcmp(expected[i], actual[i])) {
+			printf("[%sX%s] %s\n", colors.red, colors.reset, funcname);
+			printf(" ├─ expected: ");
+			str_array_to_console(expected, size);
+			printf(" └── but was: ");
+			str_array_to_console(actual, size);
 			okay = 0;
 		}
 	}
--- 2020/tools/tools.h
+++ 2020/tools/tools.h
@@ -6,7 +6,12 @@
 #define ASSERT_EQUAL(expected, actual, size) \
 	assert_equal(expected, actual, size, __FUNCTION__)
 
+#define ASSERT_STR_EQUAL(expected, actual, size) \
+	assert_str_equal(expected, actual, size, __FUNCTION__)
+
 void assert_equal(int* expected, int* actual, size_t size, const char* funcname);
+void assert_str_equal(char** expected, char** actual, size_t size, const char* funcname);
 void array_to_console(int* array, size_t size);
+void str_array_to_console(char** array, size_t size);
 
 #endif
