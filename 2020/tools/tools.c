9d6ee8f0863788ed3d649cffd1e0b5214975fef5
codaishin <codaishin@gmail.com>
2020-12-08 11:28:45 +0000

    2020/tools: Formatting of faile and success output

--- 2020/tools/tools.c
+++ 2020/tools/tools.c
@@ -16,12 +16,15 @@ void assert_equal(int* expected, int* actual, size_t size, const char* funcname)
 	int okay = 1;
 	for (size_t i = 0; i < size && okay; ++i) {
 		if (expected[i] != actual[i]) {
-			printf("%s: FAILED\n", funcname);
-			printf("expected: ");
+			printf("[\033[0;31mX\033[0m] %s\n", funcname);
+			printf(" ├─ expected: ");
 			array_print(expected, size);
-			printf("actual:   ");
+			printf(" └── but was: ");
 			array_print(actual, size);
 			okay = 0;
 		}
+	}
+	if (okay) {
+		printf("[\033[0;32m✓\033[0m] %s\n", funcname);
 	}
 }
